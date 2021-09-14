a357915b3bbd8c9f54203ad9798951694db0f198
Felicia Valentine <felice.k.valentine@gmail.com>
2021-09-14 17:23:57 +0100

    Page I think finished

--- src/components/Mainfeed/Mainfiller.jsx
+++ src/components/Mainfeed/Mainfiller.jsx
@@ -2,5 +2,7 @@ import filler from "./mainpagepictures/sayhi.png";
 
 export default function Filler (){
     return(
-<div className="mainpagefiller"> <img src={filler} alt="filler"/></div>
+<div className="mainpagefiller"> <img src={filler} alt="filler"/>
+
+</div>
 )}
