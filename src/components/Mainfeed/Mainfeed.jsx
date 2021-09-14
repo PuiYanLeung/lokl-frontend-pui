c428d6f3ce63a92e1beef9e8044dcf1471e78057
Felicia Valentine <felice.k.valentine@gmail.com>
2021-09-14 16:52:45 +0100

    random user image function

--- src/components/Mainfeed/Mainfeed.jsx
+++ src/components/Mainfeed/Mainfeed.jsx
@@ -1,6 +1,7 @@
 import "./Mainfeed.css";
-import Happyguy from "./mainpagepictures/happyguy.png";
 import Sayhi from "./mainpagepictures/sayhi.png";
+import RandomPicture from "./randompic";
+
 
 const Message = ({author, content, city, date}) =>{
     return (
@@ -21,7 +22,7 @@ export default function Mainfeed({post}) {
             <div className="Mainleft">
 
             <div className ="Banner"> <img src={Sayhi} alt="banner"/></div>
-            <div className="happyguypicture"> <img src={Happyguy} alt="HAPPYGUY"/></div>
+           <RandomPicture/>
             
             </div>
         
