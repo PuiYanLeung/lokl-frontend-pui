740aee67ce71a30196c5155c402986a83f98efa9
kjkent <kris@kriskent.com>
2021-09-15 16:29:39 +0100

    Profile now shows username

--- src/components/Profilecontent/Profilecontent.jsx
+++ src/components/Profilecontent/Profilecontent.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import "./profilecontent.css"
-import Profileguy from "./Profileguy.png"
-import { Message } from '../Mainfeed/Mainfeed'
-import Aboutmebox from './About/about'
+import React from "react";
+import "./profilecontent.css";
+import Profileguy from "./Profileguy.png";
+import {Message} from "../Mainfeed/Mainfeed";
+import Aboutmebox from "./About/about";
 
-const Greeting = () =>{
-    return(
-        <h3>Hello</h3>
-    )
-}
+const Greeting = ({user}) => {
+    return <h3>Hello, {user}!</h3>;
+};
 
-const PostH =(props)=>{
-    return(
-        <p> This is a place for message history{props.message}</p>
-    )
-}
-export default function Profilecontent(post) {
+const PostH = (props) => {
+    return <p> This is a place for message history{props.message}</p>;
+};
+export default function Profilecontent({user}) {
     return (
         <div className="Profile">
             <div className="ProfilePic">
-                <div className="happyguypicture"> <img src={Profileguy} alt="Profilepicture"/></div>
-            </div>
-            
-                <div className="ProfileContent">
-                    <div className="Greeting"><Greeting/></div>
-                   <div className="Nameprof"><Message author={post.author}/></div>
-        
-                    <div className="About"><Aboutmebox/></div>
-                    
-                    <div className="Posthistory"><PostH content={post.content}/></div>
-
+                <div className="happyguypicture">
+                    <img src={Profileguy} alt="Profilepicture" />
                 </div>
+            </div>
+            <div className="ProfileContent">
+                <div className="Greeting">
+                    <Greeting user={user.username} />
+                </div>
+                <div className="Nameprof">
+                    <Message />
+                </div>
+                <div className="About">
+                    <Aboutmebox />
+                </div>
+                <div className="Posthistory">
+                    <PostH />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
